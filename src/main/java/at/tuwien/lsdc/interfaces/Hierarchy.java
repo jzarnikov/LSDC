0125787ca45250af7fd947901d45b9769a03703e
Jan Zarnikov <comar@comar-desktop.(none)>
2010-06-03 16:46:46 +0200

    some javadoc

--- src/main/java/at/tuwien/lsdc/interfaces/Hierarchy.java
+++ src/main/java/at/tuwien/lsdc/interfaces/Hierarchy.java
@@ -2,7 +2,16 @@ package at.tuwien.lsdc.interfaces;
 
 public interface Hierarchy {
 
+    /**
+     * Return all available topics in the hierarchy.
+     * @return
+     */
     String[] allTopics();
 
+    /**
+     * Returns the name of the parent topic.
+     * @param topic an existing topic
+     * @return the name of the parent or null if a) topic does not exist or b) topic is the root topic
+     */
     String getParentOf(String topic);
 }
